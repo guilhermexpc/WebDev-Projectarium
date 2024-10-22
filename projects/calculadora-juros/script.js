a97e5af43aa2fad70ed7203e8296d8ec1c3571b3
Guilherme <guilhermee1219@gmail.com>
2024-10-22 19:44:52 -0300

    add js logic on calculadora-juros

---
+++ projects/calculadora-juros/script.js
@@ -0,0 +1,34 @@
+const eInitialValue = document.getElementById("initial-value");
+const eFee = document.getElementById("fee");
+const eTime = document.getElementById("time");
+const eTotal = document.getElementById("total");
+const feeType = document.getElementsByName("radioFee");
+function jurosCompostos() {
+    const value = eInitialValue.value;
+    const fee = eFee.value / 100;
+    const time = eTime.value;
+    return value * (1 + fee) ** time;
+}
+
+function jurosSimples() {
+    const value = eInitialValue.value;
+    const fee = eFee.value / 100;
+    const time = eTime.value;
+    const juros = value * fee * time;
+    return Number(value) + juros;
+}
+
+function CalcularJuros() {
+    const feeTypeSelected = document.querySelector('#feeType input[name="radioFee"]:checked').value;  
+    const total = feeTypeSelected == 0 ? jurosSimples() : jurosCompostos();
+    eTotal.textContent = ("R$" + total.toFixed(2).replace(".", ","));
+    // eTotal.innerHTML = ("R$" + total.toFixed(2).replace(".", ","));
+    // // eTotal.innerHTML = ("R$" + jurosCompostos().toFixed(2).replace(".", ","));
+}
+
+document.getElementById("calculate").addEventListener("click", CalcularJuros);
+feeType.forEach(element => {
+    element.addEventListener("click", CalcularJuros);
+});
+
+
