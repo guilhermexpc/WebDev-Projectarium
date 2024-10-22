ecaf7316b58345d782145332dfc04accf4aab2c5
Guilherme <guilhermee1219@gmail.com>
2024-10-22 20:16:58 -0300

    fix: adjust reset.css imports and standardize font
    - Corrected the imports of reset.css in the 'calculator' and 'digital clock' projects.
    
    - Standardized the font in both projects to ensure visual consistency.

--- projects/calculadora-juros/script.js
+++ projects/calculadora-juros/script.js
@@ -3,32 +3,33 @@ const eFee = document.getElementById("fee");
 const eTime = document.getElementById("time");
 const eTotal = document.getElementById("total");
 const feeType = document.getElementsByName("radioFee");
+
 function jurosCompostos() {
-    const value = eInitialValue.value;
-    const fee = eFee.value / 100;
-    const time = eTime.value;
-    return value * (1 + fee) ** time;
+  const value = eInitialValue.value;
+  const fee = eFee.value / 100;
+  const time = eTime.value;
+  return value * (1 + fee) ** time;
 }
 
 function jurosSimples() {
-    const value = eInitialValue.value;
-    const fee = eFee.value / 100;
-    const time = eTime.value;
-    const juros = value * fee * time;
-    return Number(value) + juros;
+  const value = eInitialValue.value;
+  const fee = eFee.value / 100;
+  const time = eTime.value;
+  const juros = value * fee * time;
+  return Number(value) + juros;
 }
 
 function CalcularJuros() {
-    const feeTypeSelected = document.querySelector('#feeType input[name="radioFee"]:checked').value;  
-    const total = feeTypeSelected == 0 ? jurosSimples() : jurosCompostos();
-    eTotal.textContent = ("R$" + total.toFixed(2).replace(".", ","));
-    // eTotal.innerHTML = ("R$" + total.toFixed(2).replace(".", ","));
-    // // eTotal.innerHTML = ("R$" + jurosCompostos().toFixed(2).replace(".", ","));
+  const feeTypeSelected = document.querySelector('#feeType input[name="radioFee"]:checked').value;
+  const total = feeTypeSelected == 0 ? jurosSimples() : jurosCompostos();
+  eTotal.textContent = ("R$" + total.toFixed(2).replace(".", ","));
+  // eTotal.innerHTML = ("R$" + total.toFixed(2).replace(".", ","));
+  // // eTotal.innerHTML = ("R$" + jurosCompostos().toFixed(2).replace(".", ","));
 }
 
 document.getElementById("calculate").addEventListener("click", CalcularJuros);
 feeType.forEach(element => {
-    element.addEventListener("click", CalcularJuros);
+  element.addEventListener("click", CalcularJuros);
 });
 
 
